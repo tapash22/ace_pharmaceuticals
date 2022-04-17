e86d0ae886b6887a8aa9c25010d2c97420c29cb2
tapash22 <tapasp263@gmail.com>
2022-04-17 12:29:13 +0600

    update again

--- src/services/Carousel.vue
+++ src/services/Carousel.vue
@@ -1,19 +1,19 @@
 <template>
-<div class="row">
-  <div class="col-md-12 caro">
-    <Carousel :items-to-show="1" :autoplay="4000" :wrap-around="true">
-      <Slide v-for="slide in products" :key="slide.id">
-        <router-link :to="slide.url">
+  <div class="row">
+    <div class="col-md-12 caro">
+      <Carousel :items-to-show="1" :autoplay="4000" :wrap-around="true">
+        <Slide v-for="slide in products" :key="slide.id">
+          <!-- <router-link :to="slide.url"> -->
           <div class="card">
             <img :src="slide.img" />
 
             <p>{{ slide.title }}</p>
           </div>
-        </router-link>
-      </Slide>
-    </Carousel>
+          <!-- </router-link> -->
+        </Slide>
+      </Carousel>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -29,19 +29,16 @@ export default {
           id: 1,
           title: "Aqua",
           img: require("../assets/image/a1.png"),
-          url: "/enzymes_poultry",
         },
         {
           id: 2,
           title: "Cattle",
           img: require("../assets/image/c1.png"),
-          url: "/essentialoil_poultry",
         },
         {
           id: 3,
           title: "Poultry",
           img: require("../assets/image/p1.png"),
-          url: "/feedsanitizer_poultry",
         },
       ],
     };
@@ -57,45 +54,46 @@ export default {
 
 <style scoped>
 .row {
-    widows: 100%;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-  }
-   .row .car {
-    display: none;
-  }
-  .row .caro {
-    width: 100%;
-    height: 250px;
-    padding: 5px;
-    margin: 0;
-    display: block;
-  }
-  .row .caro a {
-    padding: 0;
-    margin: 0;
-    text-decoration: none;
-  }
-  .row .caro a .card {
-    width: 100%;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-    border: none;
-  }
-  .row .caro a .card img {
-    widows: 100%;
-    height: 90%;
-    padding: 0;
-    margin: 0;
-  }
-   .row .caro a .card p {
-    width: 100%;
-    height: 10%;
-font-size: 1.5rem;
-    font-weight: 600;
-    font-family: "Playfair Display", serif;
-    color: #000;
-  }
+  widows: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+.row .car {
+  display: none;
+}
+.row .caro {
+  width: 100%;
+  height: 250px;
+  padding: 5px;
+  margin: 0;
+  display: block;
+}
+.row .caro a {
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+}
+.row .caro a .card {
+  width: 100%;
+
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  border: none;
+}
+.row .caro a .card img {
+  widows: 100%;
+  height: 90%;
+  padding: 0;
+  margin: 0;
+}
+.row .caro .card p {
+  width: 100%;
+  height: 10%;
+  font-size: 1.5rem;
+  font-weight: 600;
+  font-family: "Playfair Display", serif;
+  color: #000;
+}
 </style>
